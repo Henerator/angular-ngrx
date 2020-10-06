25e14c812da1d6363b1da1e1cd6c80a624081062
roman makarov <roman_makarov@epam.com>
2020-10-06 18:33:31 +0400

    update initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ngrx';
+  names = [
+    'name1',
+    'name2',
+    'name3',
+    'name4',
+  ];
 }
